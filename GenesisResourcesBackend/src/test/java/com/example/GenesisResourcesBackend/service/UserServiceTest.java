f22ee57d427a7d47b2a4cae586035d33ee2d6fb7
davidStupen <davidstupen4@seznam.cz>
2025-10-24 11:38:43 +0200

    save testy

--- GenesisResourcesBackend/src/test/java/com/example/GenesisResourcesBackend/service/UserServiceTest.java
+++ GenesisResourcesBackend/src/test/java/com/example/GenesisResourcesBackend/service/UserServiceTest.java
@@ -1,11 +1,18 @@
 package com.example.GenesisResourcesBackend.service;
 
+import com.example.GenesisResourcesBackend.modul.User;
 import com.example.GenesisResourcesBackend.repository.UserRepo;
 import com.example.GenesisResourcesBackend.service.certificate.CertificateAuthority;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
@@ -19,11 +26,25 @@ class UserServiceTest {
         this.authority = new CertificateAuthority();
         this.userService = new UserService(this.userRepo, this.authority);
     }
-
+//      rU8nA9eT2bYh
+//      wV6eH1fK7qZj
+//      sL4gN9dC3bXz
+    //DataIntegrityViolationException
     @Test
-    void saveUser() {
+    void saveUserTrue() {
+        this.userRepo.saveAll(List.of(
+                new User("David", "Stupen", "rU8nA9eT2bYh", UUID.randomUUID().toString()),
+                new User("Jarda", "Jagr", "wV6eH1fK7qZj", UUID.randomUUID().toString())
+        ));
+        assertTrue(this.userRepo.count() > 0);
     }
-
+    @Test
+    void saveThrowException(){
+        List<User> users = Arrays.asList(
+                new User("David", "Stupen", "rU8nA9eT2bYh", UUID.randomUUID().toString()),
+                new User("Jarda", "Jagr", "rU8nA9eT2bYh", UUID.randomUUID().toString()));
+        assertThrows(DataIntegrityViolationException.class, () -> this.userRepo.saveAll(users));
+    }
     @Test
     void getUserById() {
     }
